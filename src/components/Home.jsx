51ff13e41b34272c910171f7ec918ed25056661f
dp-shetty <dps2k811@gmail.com>
2024-07-03 18:08:22 +0530

    completed 75% of homepage work remained with about me button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "../SCSS/Home.scss"
 import image from "../assets/images/dps.png"
+import {useSelector } from 'react-redux';
+
+
 
 
 
 function Home() {
+
+  let {bodyBg} = useSelector(state=>state.iconBg)
+  let middleContentRef = useRef()
+  let bodyContentRef = useRef() 
+
+
+    useEffect(()=>{
+
+      if(bodyBg==="#111111"){
+        middleContentRef.current.style.color="white"
+        bodyContentRef.current.style.color="white"
+      }else if(bodyBg==="#ffffff"){
+        middleContentRef.current.style.color="black"
+        bodyContentRef.current.style.color="black"
+      }
+
+    },[bodyBg])
+
+
   return (
-    <>
+    <section id="homeSec">
     <div id="profileImgDiv">
     <div id="imageDiv">
     <img src={image} alt="" />
@@ -14,9 +36,27 @@ function Home() {
     </div>
 
     <div id="homeContent">
-    {/* content */}
+    <p id='headContent' ref={middleContentRef} style={{fontSize:"40px",fontWeight:"600"}}>
+     <span>
+     <hr id='hleId'/>
+     I'M DURGA PRASAD SHETTY.
+     <br />
+     </span>
+     <p id='middleContent' style={{marginLeft:"2.8rem"}}>
+     REACT WEB DEVELOPER
+     </p>
+    </p>
+    <br />
+    <p id='bodyContent' ref={bodyContentRef} style={{marginLeft:"2.8rem",fontSize:"20px"}}>
+    Creative and passionate Frontend React Developer with a knack for crafting seamless
+    <br />
+    and dynamic user experiences. Skilled in turning complex problems into intuitive and responsive designs,
+    I build modern web applications that delight users and drive
+    <br />
+     engagement.
+    </p>
     </div>
-    </>
+    </section>
   )
 }
 
